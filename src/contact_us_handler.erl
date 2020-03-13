6431015d7a113e897df73cfb6bcb297749abe407
Erik Hansen <e.hansen31@live.com>
2020-03-13 00:46:23 -0400

    returning body sent in to endpoint

--- src/contact_us_handler.erl
+++ src/contact_us_handler.erl
@@ -5,10 +5,13 @@
 -export([init/2]).
 
 init(Req0 = #{method := <<"POST">>}, State) ->
-    Req = cowboy_req:reply(200,
-			   #{<<"content-type">> => <<"text/plain">>},
-			   <<"Hello world!">>, Req0),
-    {ok, Req, State};
+    {ok, Data, Req1} = cowboy_req:read_body(Req0),
+    % logger:error("error happened because: ~p", Data),
+    % BodyMap = jiffy:decode(Data),
+    Req2 = cowboy_req:reply(200,
+			    #{<<"content-type">> => <<"text/plain">>}, Data,
+			    Req0),
+    {ok, Req2, State};
 init(Req0, State) ->
     Req = cowboy_req:reply(405,
 			   #{<<"allow">> => <<"POST">>}, Req0),
