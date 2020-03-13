2de45c033859e295cac39c5ecfd9f47ba1394076
Erik Hansen <e.hansen31@live.com>
2020-03-12 23:20:54 -0400

    getting a response from post

--- src/contact_us_handler.erl
+++ src/contact_us_handler.erl
@@ -1,48 +1,15 @@
 -module(contact_us_handler).
 
--behavior(cowboy_rest).
+-behavior(cowboy_handler).
 
 -export([init/2]).
 
--export([allowed_methods/2]).
-
--export([content_types_accepted/2]).
-
--export([contact_us/2]).
-
-init(Req, State) -> {cowboy_rest, Req, State}.
-
-content_types_accepted(Req, State) ->
-    {[{<<"application/json">>, contact_us}], Req, State}.
-
-allowed_methods(Req, State) ->
-    {[<<"POST">>], Req, State}.
-
-contact_us(Req, State) ->
-    Req1 = cowboy_req:reply(200,
-			    #{<<"content-type">> => <<"text/plaid">>},
-			    jiffy:encode(<<"Body">>), Req),
-    {ok, Req1,
-     State}.    % {ok, Body, Req1} = cowboy_req:read_urlencoded_body(Req),
-		% case Body of
-		%   [{Input, true}] ->
-
-        %   Req1 = cowboy_req:reply(200,
-	% 			  #{<<"content-type">> =>
-	% 				<<"application/json">>},
-	% 			  jiffy:encode(<<"Body">>), Req),
-	%   {ok, Req1, State};
-    %   [] ->
-
-        %   Req1 = cowboy_req:reply(400,
-	% 			  #{<<"content-type">> =>
-	% 				<<"application/json">>},
-	% 			  jiffy:encode(<<"Missing body">>), Req);
-    %   _ ->
-
-        %   Req1 = cowboy_req:reply(400,
-	% 			  #{<<"content-type">> =>
-	% 				<<"application/json">>},
-	% 			  jiffy:encode(<<"Bad Request">>), Req)
-    % end.
-
+init(Req0 = #{method := <<"POST">>}, State) ->
+    Req = cowboy_req:reply(200,
+			   #{<<"content-type">> => <<"text/plain">>},
+			   <<"Hello world!">>, Req0),
+    {ok, Req, State};
+init(Req0, State) ->
+    Req = cowboy_req:reply(405,
+			   #{<<"allow">> => <<"POST">>}, Req0),
+    {ok, Req, State}.
--- src/erlang_api_app.erl
+++ src/erlang_api_app.erl
@@ -11,8 +11,9 @@
 
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([{'_',
-				       [{"/", hello_handler, []}],
-				       [{"/contact", contact_us_handler, []}]}]),
+				       [{"/", hello_handler, []},
+					{"/contact", contact_us_handler,
+					 []}]}]),
     {ok, _} = cowboy:start_clear(my_http_listener,
 				 [{port, 8080}],
 				 #{env => #{dispatch => Dispatch}}),
