92a9d9f12ed0a1cbdbc6600bf11cf10eb96bc732
Erik Hansen <e.hansen31@live.com>
2020-03-15 19:53:47 -0400

    start on middleware unit test

--- src/handlers/middleware.erl
+++ src/handlers/middleware.erl
@@ -1,5 +1,23 @@
 -module(middleware).
 
--export([]).
+-export([authorized/1]).
 
-authorized(Req, State) -> ok.
+authorized(Req) ->
+    Auth = cowboy_req:header(<<"authorization">>, Req, nil),
+    {ok, [{<<"uuid">>, Uuid}]} = jwt:decode(Auth,
+					    <<"key">>),
+    accounts_db:get_account(Uuid),
+    % place account in response
+    {true, Req}.
+
+% meck on db call for testing...
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+authorized_test() -> 
+    jwt:encode(),
+authorized(#{<<"authorization">>=> ""}), ok.
+
+-endif.
