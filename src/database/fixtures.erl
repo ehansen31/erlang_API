78978e2b8f96bed3aea43efb8979bbff64c7b92b
Erik Hansen <e.hansen31@live.com>
2020-03-21 14:00:38 -0400

    pool with workers running

--- src/database/fixtures.erl
+++ src/database/fixtures.erl
@@ -3,10 +3,18 @@
 -export([inject/1]).
 
 inject(Conn) ->
-    {ok, _} = epgsql:squery(Conn,
-			   "Insert INTO accounts(email, uuid,created_"
-			   "at, updated_at) VALUES ('e.hansen31@live.c"
-			   "om','b06c1b51-da53-43ce-ae4d-ac52ba9da938', "
-			   "'2020-03-17 21:50:46', '2020-03-17 21:50:46')"
-			   ";"),
+    Worker = poolboy:checkout(default_pool),
+    erlang_api_app:squery(Worker,
+			  "Insert INTO accounts(email, uuid,created_at, "
+			  "updated_at) VALUES ('e.hansen31@live.com','b0"
+			  "6c1b51-da53-43ce-ae4d-ac52ba9da938', "
+			  "'2020-03-17 21:50:46', '2020-03-17 21:50:46')"
+			  ";"),
+    poolboy:checkin(default_pool, Worker),
+    % {ok, _} = epgsql:squery(Conn,
+    % 		   "Insert INTO accounts(email, uuid,created_"
+    % 		   "at, updated_at) VALUES ('e.hansen31@live.c"
+    % 		   "om','b06c1b51-da53-43ce-ae4d-ac52ba9da938', "
+    % 		   "'2020-03-17 21:50:46', '2020-03-17 21:50:46')"
+    % 		   ";"),
     ok.
--- src/erlang_api_app.erl
+++ src/erlang_api_app.erl
@@ -7,7 +7,13 @@
 
 -behaviour(application).
 
--export([init/1, start/2, stop/1]).
+-behaviour(supervisor).
+
+-export([equery/3, squery/2, start/0, stop/0]).
+
+-export([start/2, stop/1]).
+
+-export([init/1]).
 
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([{'_',
@@ -17,20 +23,38 @@ start(_StartType, _StartArgs) ->
     {ok, _} = cowboy:start_clear(my_http_listener,
 				 [{port, 8080}],
 				 #{env => #{dispatch => Dispatch}}),
-    erlang_api_sup:start_link().
+    % erlang_api_sup:start_link(),
+    supervisor:start_link({local, erlang_api_sup}, ?MODULE,
+			  []).
+
+start() -> application:start(?MODULE).
+
+stop() -> application:stop(?MODULE).
 
 stop(_State) -> ok.
 
 %% internal functions
 
 init([]) ->
-    {ok, Pools} = application:get_env(example, pools),
+    {ok, Pools} = application:get_env(erlang_api, pools),
     PoolSpecs = lists:map(fun ({Name, SizeArgs,
 				WorkerArgs}) ->
 				  PoolArgs = [{name, {local, Name}},
-					      {worker_module, example_worker}]
+					      {worker_module,
+					       erlang_api_worker}]
 					       ++ SizeArgs,
 				  poolboy:child_spec(Name, PoolArgs, WorkerArgs)
 			  end,
 			  Pools),
     {ok, {{one_for_one, 10, 10}, PoolSpecs}}.
+
+squery(PoolName, Sql) ->
+    poolboy:transaction(PoolName,
+			fun (Worker) -> gen_server:call(Worker, {squery, Sql})
+			end).
+
+equery(PoolName, Stmt, Params) ->
+    poolboy:transaction(PoolName,
+			fun (Worker) ->
+				gen_server:call(Worker, {equery, Stmt, Params})
+			end).
