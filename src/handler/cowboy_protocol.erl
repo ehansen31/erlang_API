f063c63a5c157f5a597aab4a2a13643838267532
Erik Hansen <e.hansen31@live.com>
2020-03-29 13:08:43 -0400

    it's werking!

--- src/handler/cowboy_protocol.erl
+++ src/handler/cowboy_protocol.erl
@@ -1,15 +1,43 @@
 -module(cowboy_protocol).
 
--export([init/4, start_link/4]).
+-behaviour(ranch_protocol).
 
-start_link(ListenerPid, Socket, Transport, Opts) ->
+-export([start_link/4]).
+
+-export([init/4]).
+
+start_link(Ref, Socket, Transport, Opts) ->
     Pid = spawn_link(?MODULE, init,
-		     [ListenerPid, Socket, Transport, Opts]),
+		     [Ref, Socket, Transport, Opts]),
     {ok, Pid}.
 
-init(ListenerPid, Socket, Transport, Opts) ->
-    [{env, [{dispatch, Dispatch}]}] = Opts,
-    cowboy:start_tls(ListenerPid,
-		     #{socket_opts => Socket, transport_opts => Transport},
-		     #{env => #{dispatch => Dispatch}}),
-    ok.
+init(Ref, Socket, Transport, _Opts) ->
+    ok = ranch:accept_ack(Ref), loop(Socket, Transport).
+
+loop(Socket, Transport) ->
+    case Transport:recv(Socket, 0, 5000) of
+      {ok, Data} ->
+	  io:format("data:\n~p", [Data]),
+	  Transport:send(Socket, Data),
+	  loop(Socket, Transport);
+      _ -> ok = Transport:close(Socket)
+    end.
+
+% -export([init/4, start_link/4]).
+
+% start_link(ListenerPid, Socket, Transport, Opts) ->
+%     Pid = spawn_link(?MODULE, init,
+% 		     [ListenerPid, Socket, Transport, Opts]),
+%     {ok, Pid}.
+
+% init(ListenerPid, Socket, Transport, Opts) ->
+%     [{env, [{dispatch, Dispatch}]}] = Opts,
+%     % cowboy:start_tls(ListenerPid,
+%     % 	     #{socket_opts => Socket, transport_opts => Transport},
+%     % 	     #{env => #{dispatch => Dispatch}}),
+%     cowboy:start_clear(cowboy_api_now,
+% 		          #{socket_opts => Socket, transport_opts => Transport},
+% 		    %    [{port, 8080}, {ip, {127, 0, 0, 1}}],
+% 		       #{env => #{dispatch => Dispatch}}),
+%     ok.
+
